876b27f1a795657c9ef26e1a08ef1a808567c935
Michael <1michaelfang@gmail.com>
2017-11-15 18:18:46 -0800

    asdf

--- playtest.py
+++ playtest.py
@@ -1,7 +1,7 @@
 import pygame
 import time
 
-pygame.init()
-pygame.mixer.music.load('./music/1980/1.mp3')
-pygame.mixer.music.play()
-time.sleep(100)
+pygame.mixer.init()
+sound = pygame.mixer.Sound('./music/1980/1.wav')
+while True:
+    sound.play()
